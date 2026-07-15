4bc7b8244512a57922646be2d1845484aba2bdb5
Lee JaeHyun <myleje@gmail.com>
2026-07-15 19:15:20 +0900

    .

---
+++ CSharp_02/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharp_02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 3;
+
+        // int a = 10, b = 3;
+        // 산술 연산자 (+ - / * %)
+        Console.WriteLine(a + b);
+        Console.WriteLine(a - b);
+    }
+}
