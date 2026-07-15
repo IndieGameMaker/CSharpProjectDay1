83c40aa51257439f58f7e0561857688efac72203
Lee JaeHyun <myleje@gmail.com>
2026-07-15 19:22:56 +0900

    chore: add multiplication, division, and modulo operations in Program.cs

--- CSharp_02/Program.cs
+++ CSharp_02/Program.cs
@@ -11,5 +11,17 @@ class Program
         // 산술 연산자 (+ - / * %)
         Console.WriteLine(a + b);
         Console.WriteLine(a - b);
+        Console.WriteLine(a * b);
+        Console.WriteLine(a / b); // 정수 / 정수 ==> 소숫점 10/3 3.333
+
+        float c = 10.0f; // 10f;
+        float d = 3.0f;  // 3f;
+        Console.WriteLine(c / d);
+        
+        // 0 으로 나누는 경우 ==> 주의
+        Console.WriteLine(c / 0);
+        
+        // 나머지 연산 (Modulo)
+        Console.WriteLine(a % b);
     }
 }
