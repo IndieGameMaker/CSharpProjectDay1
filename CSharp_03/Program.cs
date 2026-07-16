87da82f0b47263dec048bf9f807ac3b7e7ecd9ad
Lee JaeHyun <myleje@gmail.com>
2026-07-16 19:15:15 +0900

    chore: add monster rank classification based on level in Program.cs

--- CSharp_03/Program.cs
+++ CSharp_03/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace CSharp_03;
+﻿// ReSharper disable All
+namespace CSharp_03;
 
 class Program
 {
@@ -9,6 +10,27 @@ class Program
         
         // 몬스터의 등급
         string rank;
+
+        // bool isGameOver = true;
+        // if (isGameOver) return;
         
+        if (monsterLevel >= 50)
+        {
+            rank = "보스";
+        }
+        else if (monsterLevel >= 30)
+        {
+            rank = "정예";
+        }
+        else if (monsterLevel >= 10)
+        {
+            rank = "일반";
+        }
+        else
+        {
+            rank = "잡몹";
+        }
+        
+        Console.WriteLine($"레벨: {monsterLevel} 몬스터 등급: {rank}");
     }
 }
