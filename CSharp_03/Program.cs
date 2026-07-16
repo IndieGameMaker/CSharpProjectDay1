4a94115fdfb02db7568b09bd410cc1525dd7e3bc
Lee JaeHyun <myleje@gmail.com>
2026-07-16 19:31:54 +0900

    chore: define MonsterRank enum and update rank assignment logic in Program.cs

--- CSharp_03/Program.cs
+++ CSharp_03/Program.cs
@@ -1,6 +1,12 @@
 ﻿// ReSharper disable All
 namespace CSharp_03;
 
+// 열거형 정의
+enum MonsterRank
+{
+    Boss, Elite, Normal, Weak
+}
+
 class Program
 {
     static void Main(string[] args)
@@ -9,28 +15,37 @@ class Program
         int monsterLevel = 47;
         
         // 몬스터의 등급
-        string rank;
+        MonsterRank rank;
 
         // bool isGameOver = true;
         // if (isGameOver) return;
         
         if (monsterLevel >= 50)
         {
-            rank = "보스";
+            rank = MonsterRank.Boss;
         }
         else if (monsterLevel >= 30)
         {
-            rank = "정예";
+            rank = MonsterRank.Elite;
         }
         else if (monsterLevel >= 10)
         {
-            rank = "일반";
+            rank = MonsterRank.Normal;
         }
         else
         {
-            rank = "잡몹";
+            rank = MonsterRank.Weak;
         }
         
         Console.WriteLine($"레벨: {monsterLevel} 몬스터 등급: {rank}");
+
+        int reward = rank switch
+        {
+            MonsterRank.Boss => 1000,
+            MonsterRank.Elite => 300,
+            MonsterRank.Weak => 100,
+            _ => 30
+        };
+        Console.WriteLine($"처치 보상: {reward} G");
     }
 }
