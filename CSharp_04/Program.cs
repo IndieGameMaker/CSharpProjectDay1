68d1eec5b8d04ab7fe681331279f2f75b0ec032d
Lee JaeHyun <myleje@gmail.com>
2026-07-20 19:27:10 +0900

    chore: add inventory management example in CSharp_04 with List usage in Program.cs

---
+++ CSharp_04/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CSharp_04;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // List로 인벤토리 만들기
+        List<string> inventory = new List<string>();
+        
+        // 아이템 추가
+        inventory.Add("목검");
+        inventory.Add("녹슨 검");
+        inventory.Add("체력 물약");
+        
+        Console.WriteLine($"인벤토리 갯수 : {inventory.Count}");
+        // 아이템 확인
+        // var 키워드
+        
+        foreach (string item in inventory)
+        {
+            Console.WriteLine($"{inventory.IndexOf(item)} : {item}");
+        }
+        
+        // 녹슨 검 버리기(List 삭제)
+        inventory.Remove("녹슨 검");
+        Console.WriteLine($"검 버린 후 갯수: {inventory.Count}");
+        
+        // 물약 검색
+        if (inventory.IndexOf("체력 물약") > -1)
+        {
+            Console.WriteLine($"물약 보유");
+        }
+        
+        
+    }
+}
