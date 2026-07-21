4de100907ec5521f336f9723662555154154c1c8
Lee JaeHyun <myleje@gmail.com>
2026-07-21 19:55:52 +0900

    chore: encapsulate hp field in Player class and add getter/setter methods

--- CSharp_05/Program.cs
+++ CSharp_05/Program.cs
@@ -1,21 +1,39 @@
 ﻿namespace CSharp_05;
 
+/*
+ * 접근제한자
+ * public  - 어디서든지 모두 접근을 허용 (외부에 공개)
+ * private - 자신의 클래스에서 만 접근을 허용 (데이터 은닉화, 캡슐화)
+ * protected - 자신의 클래스 + 자식 클래스에 접근을 허용
+ * internal - 같은 어셈블리 안에서 접근을 허용 (같은 프로그램내에서 접근을 허용)
+ */
+
 // 클래스 정의 (설계도)
-class Player
+internal class Player
 {
     // 필드(field) : 데이터
     public string name;
-    public float hp;
+    private float hp; // 캡슐화
     public float attackPower;
 
+    public void SetHp(float hp)
+    {
+        this.hp = hp;
+    }
+    
+    public float GetHp()
+    {
+        return hp;
+    }
+    
     // 메서드(Method) : 로직 (동작)
     public void Attack()
     {
-        Console.WriteLine($"{name} 가 {attackPower}로 공격!");
+        Console.WriteLine($"{name}가 {attackPower} 파워로 공격!");
     }
 }
 
-class Program
+internal class Program
 {
     static void Main(string[] args)
     {
@@ -25,7 +43,10 @@ class Program
         Player hero = new Player();
         // 객체의 필드값 정의 (변수 설정)
         hero.name = "전사";  // 객체.필드
-        hero.hp = 100f;
+        // hero.hp = 100f;
+        hero.SetHp(100f);
+        float hp = hero.GetHp();
+        Console.WriteLine(hp);
         hero.attackPower = 30f;
         // 메소드 호출
         hero.Attack();
