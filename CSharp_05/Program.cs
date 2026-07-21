20e0a471214869d01e8bd784e07c043233e53621
Lee JaeHyun <myleje@gmail.com>
2026-07-21 20:23:45 +0900

    chore: refactor hp field in Player class to use property with validation

--- CSharp_05/Program.cs
+++ CSharp_05/Program.cs
@@ -9,22 +9,46 @@
  */
 
 // 클래스 정의 (설계도)
+// C# 코딩 컨벤션 (코드 작성할 때 표준)
 internal class Player
 {
     // 필드(field) : 데이터
     public string name;
-    private float hp; // 캡슐화
+    private float _hp; // 캡슐화
     public float attackPower;
 
-    public void SetHp(float hp)
+    // 프로퍼티 방식 (Property: 속성)
+    public float Hp
     {
-        this.hp = hp;
+        get => _hp;
+        set
+        {
+            if (value > 100f)
+            {
+                _hp = 100f;
+            }
+            else if (value < 0f)
+            {
+                _hp = 0f;
+            }
+            else
+            {
+                _hp = value;
+            }
+        }
     }
     
-    public float GetHp()
+    // 메서드를 활용한 방식
+    public void SetHp(float hp)
     {
-        return hp;
+        _hp = hp;
     }
+
+    public float GetHp() => _hp;
+    // public float GetHp()
+    // {
+    //     return _hp;
+    // }
     
     // 메서드(Method) : 로직 (동작)
     public void Attack()
@@ -44,9 +68,13 @@ internal class Program
         // 객체의 필드값 정의 (변수 설정)
         hero.name = "전사";  // 객체.필드
         // hero.hp = 100f;
-        hero.SetHp(100f);
-        float hp = hero.GetHp();
+        // hero.SetHp(100f);
+        hero.Hp = -90f; // set {}
+        
+        // 지역 변수 (Local Variable)
+        float hp = hero.Hp; // get {} // hero.GetHp();
         Console.WriteLine(hp);
+        
         hero.attackPower = 30f;
         // 메소드 호출
         hero.Attack();
