7a6007d7ec60f39b0bcc1d951a2bf360f997d8ac
Lee JaeHyun <myleje@gmail.com>
2026-07-22 19:17:41 +0900

    chore: add CSharp_06 project and initial Program class for new functionality

---
+++ CSharp_06/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CSharp_06;
+
+/*
+ * # 객체지향 프로그래밍(Object Oriented Programming)
+ *
+ * [장점]
+ * - 코드의 재사용성 높다.
+ * - 코드의 확장성 및 유지 보수성이 편리함.
+ * - 협업에 유리함 - 개발자간의 역할 분담 명확
+ * - 대규모 프로젝트에 적합
+ *
+ * [단점]
+ * - 초기 클래스 설계 비용이 크다.
+ * - 잘못된 클래스 설계는 유지 보수를 어렵게한다.
+ * - God Object (클래스가 비대해지는 것을 경계)
+ * - 과도한 추상화의 위험 (상속 남발 -> 구조가 복잡)
+ * - 객체의 생성 비용 발생
+ *
+ * # 객체지향 프로그래밍의 4가지 핵심원칙
+ * - 캡슐화 (Encapsulation) : 데이터를 안전하게 보호
+ * - 상속 (Inheritance) : 코드의 재사용성
+ * - 다형성 (Polymorphism) : 유연한 설계
+ * - 추상화 (Abstraction) : 복잡성 줄이기
+ */
+
+class 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
